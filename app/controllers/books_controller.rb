f8c998283e881f17026aa0b78dfe2d8f5d3c89df
vagrant <vagrant@localhost.localdomain>
2020-04-19 14:38:37 +0000

    20200419

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
-  def top
-  end
-
   def index
+    @book = Book.all
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redierct_to vook_path
   def show
   end
 
   def edit
   end
+
+  def top
+  end
+
+  private
+    def blog_params
+      params.require(:book).permit(:Title, :Body)
+    end
+
 end
